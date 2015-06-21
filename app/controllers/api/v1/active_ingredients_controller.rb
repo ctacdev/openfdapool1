02dc41576e9f630889faa2421bc839b5a43291d8
Juan Alvarado <jalvarado@ctacorp.com>
2015-06-21 10:34:34 -0400

    Add ability to filter active ingredients
    Filter active ingredients returned from the api using the `q` parameter.

--- app/controllers/api/v1/active_ingredients_controller.rb
+++ app/controllers/api/v1/active_ingredients_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ActiveIngredientsController < Api::V1::ApiController
   def index
-    @active_ingredients = ActiveIngredient.where nil
+    search = params[:q]
+    @active_ingredients = ActiveIngredient.where("name LIKE ?", "#{search}%").
+      order("name desc")
     render json: @active_ingredients
   end
 end
--- spec/requests/active_ingredients_spec.rb
+++ spec/requests/active_ingredients_spec.rb
@@ -3,9 +3,34 @@ require 'rails_helper'
 describe "ActiveIngredients API" do
   it "sends a list of active ingredients" do
     FactoryGirl.create_list :active_ingredient, 2
-    get '/api/v1/active_ingredients'
+    get "/api/v1/active_ingredients"
     expect(response).to be_success
-    json = JSON.parse response.body
     expect(json.length).to eq(2)
+  end
+
+  it "filters the results based on the search parameter q" do
+    FactoryGirl.create :active_ingredient, name: "Should show up in the list"
+    FactoryGirl.create :active_ingredient, name: "Does not show up in the list"
+
+    get "/api/v1/active_ingredients?q=Should"
+    expect(json.length).to eq(1)
+    expect(json.first.fetch("name")).to eq("Should show up in the list")
+  end
+
+  it "returns all active ingredients when passed an empty q parameter" do
+    FactoryGirl.create :active_ingredient
+    get "/api/v1/active_ingredients?q="
+    expect(json.length).to eq(1)
+  end
+
+  it "ignores case when searching for matches" do
+    FactoryGirl.create :active_ingredient, name: "ALL CAPS"
+    FactoryGirl.create :active_ingredient, name: "all downcase"
+    get "/api/v1/active_ingredients?q=ALL"
+    expect(json.length).to eq(2)
+  end
+
+  def json
+    JSON.parse response.body
   end
 end

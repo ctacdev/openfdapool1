051bf15f63bd6e4937847516ccfa6a460c656c33
Steffen Gates <sgates@ctacorp.com>
2015-06-25 23:24:16 -0400

    fixed failing test

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -10,7 +10,7 @@ feature 'Home page' do
   #   Then I see "Welcome"
   scenario 'visit the home page' do
     visit root_path
-    expect(page).to have_content 'banana'
+    expect(page).to have_content 'PharmaQuery'
   end
 
 end
